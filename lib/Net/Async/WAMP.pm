d3b2c1c549bfbf36bde89d133715264ec0ff02e4
Stephen Rollyson <srollyson@cpan.org>
2013-09-19 22:58:30 -0400

    Added scaffolding for PodWeaver-consumable documentation.

--- lib/Net/Async/WAMP.pm
+++ lib/Net/Async/WAMP.pm
@@ -3,6 +3,25 @@ use warnings;
 package Net::Async::WAMP;
 # ABSTRACT: Perl implementation of the WebSocket Application Messaging Protocol.
 
+=head1 SYNOPSIS
+
+  my $response = do_a_thing($request)->get();
+  my ($resp1, $resp2) = Future->wait_all(
+                            do_a_thing($request1),
+                            do_a_thing($request2) )->get();
+
+=method example_method
+
+TODO: Fill this out.
+
+=method another_example_method
+
+TODO: Fill this out.
+
+=head1 SEE ALSO
+
+=cut
+
 use Net::Async::WebSocket 0.07;
 
 1;
